d0ed0a9055b3638384d49c3d092066c9ebdedd86
gaudiom4git <sailfunds29@gmail.com>
2022-05-20 21:29:19 -0400

    Added cities array

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,16 +1,19 @@
-// 13.4.1
+// 13.4.2
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// get cities.js data
+let cityData = cities;   //don't need .js
+
 // Create the map object with a center and zoom level
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);  //LA
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
   // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  //id: 'mapbox/streets-v11',
-  id: 'mapbox/dark-v10',  //make dark
+  id: 'mapbox/streets-v11',
+  //id: 'mapbox/dark-v10',  //make dark
   tileSize: 512,
   zoomOffset: -1,
   accessToken: API_KEY
@@ -28,12 +31,20 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//  Loop through cities array using function
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.marker(city.location)
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population + "</h3>")
+//   .addTo(map);
+// });
 
-//Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",       //black lines
-  fillColor: '#ffffa1'  //light yellow circle
-}).addTo(map);
+//  Loop through cities array using function
+ cityData.forEach(function(city) {
+   console.log(city)
+   L.circleMarker(city.location, {
+     radius: city.population/100000
+   })
+   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population + "</h3>")
+   .addTo(map);
+ });
