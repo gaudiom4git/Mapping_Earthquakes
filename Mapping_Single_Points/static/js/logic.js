3ce8629616612fecfad5526a36f5858775dde5d4
gaudiom4git <sailfunds29@gmail.com>
2022-05-20 20:35:31 -0400

    Added markers to logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,27 +1,19 @@
+// 13.4.1
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-// geographical center of the map to the approximate geographical center of the United States.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-
-// Create the map object with a center and zoom level.
-// An alternative to using the setView()
-/* let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  }); */
+// Create the map object with a center and zoom level
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);  //LA
 
   // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: API_KEY
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  //id: 'mapbox/streets-v11',
+  id: 'mapbox/dark-v10',  //make dark
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: API_KEY
 });
 
 //  Other map ids to use isntead of streets-v11
@@ -35,3 +27,13 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",       //black lines
+  fillColor: '#ffffa1'  //light yellow circle
+}).addTo(map);
