b3c700d6a6a7f0bef8f2b7077d9679b83174bbb7
gaudiom4git <sailfunds29@gmail.com>
2022-05-22 13:38:47 -0400

    GeoJSON

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -42,20 +42,20 @@ streets.addTo(map);
 //  Add a marker to the map for Los Angeles, California.
 //let marker = L.marker([37.5, -122.5]).addTo(map);
 
-//Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  pointToLayer: function(feature, latlng) {
-    console.log(feature);
-    return L.marker(latlng)
-    .bindPopup("<h2>" + feature.properties.name + "</h2>");
-  }
-
-}).addTo(map);
-
+// //Grabbing our GeoJSON data.
 // L.geoJSON(sanFranAirport, {
-//   onEachFeature: function(feature, layer) {
-//     console.log(layer);
-//     layer.bindPopup();
-//    }
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.name + "</h2>");
+//   }
+
 // }).addTo(map);
+
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup();
+   }
+}).addTo(map);
